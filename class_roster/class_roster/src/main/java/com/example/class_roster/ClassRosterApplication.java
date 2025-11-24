aa1e3c64346baa296ada46fbd7bce22c9f50b345
tworsum4yu <52200316+tworsum4yu@users.noreply.github.com>
2025-11-24 14:16:30 +0000

    Add files via upload
    Initial commit to get Spring auto generated files

---
+++ class_roster/class_roster/src/main/java/com/example/class_roster/ClassRosterApplication.java
@@ -0,0 +1,13 @@
+package com.example.class_roster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassRosterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassRosterApplication.class, args);
+	}
+
+}
---
+++ class_roster/class_roster/src/test/java/com/example/class_roster/ClassRosterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.class_roster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassRosterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
